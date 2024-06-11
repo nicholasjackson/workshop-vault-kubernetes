c9dfe61e8f6f365bd13de82ba15b017a869bb577
Nic Jackson <jackson.nic@gmail.com>
2024-06-11 16:15:15 +0100

    update script, set service account

--- scripts.sh
+++ scripts.sh
@@ -3,53 +3,49 @@
 # Configure database secrets
 vault secrets enable database
 
-vault write database/config/payments \
-    plugin_name=postgresql-database-plugin \
+vault write database/config/chat \
+    plugin_name=mongodb-database-plugin \
     allowed_roles=writer,reader \
-    connection_url="postgresql://{{username}}:{{password}}@10.100.0.180:5432/payments?sslmode=disable" \
-    username="postgres" \
+    connection_url="mongodb://{{username}}:{{password}}@10.100.0.180:27017/admin?tls=false" \
+    username="chat" \
     password="password"
 
 # Rotate root password
-vault write -force database/rotate-root/payments
+vault write -force database/rotate-root/chat
 
 # Create the roles
 vault write database/roles/writer \
-    db_name=payments \
-    creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; 
-    GRANT SELECT, INSERT, UPDATE ON payment_card TO \"{{name}}\";" \
+    db_name=chat \
+    creation_statements='{ "db": "admin", "roles": [{ "role": "readWrite" }, {"role": "read", "db": "foo"}] }' \
     default_ttl="1h" \
     max_ttl="24h"
 
 vault write database/roles/reader \
-    db_name=payments \
-    creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; 
-    GRANT SELECT ON payment_card TO \"{{name}}\";" \
+    db_name=chat \
+    creation_statements='{ "db": "admin", "roles": [{ "role": "readWrite" }, {"role": "read", "db": "foo"}] }' \
     default_ttl="1h" \
     max_ttl="24h"
-
-
 
 # Configure Database Encryption
 vault secrets enable transit
 
 # Create the key
-vault write -f transit/keys/payments type=rsa-4096
+vault write -f transit/keys/hcat type=rsa-4096
 
 # Configure Kubernetes Auth
-vault write auth/kubernetes/role/payments \
-  bound_service_account_names=payments \
+vault write auth/kubernetes/role/chat \
+  bound_service_account_names=chat \
   bound_service_account_namespaces=default \
-  token_policies=payments
+  token_policies=chat
   ttl=24h
 
 # Create the policy
-vault policy write payments - <<EOF
+vault policy write chat - <<EOF
 path "database/creds/writer" {
   capabilities = ["read", "create"]
 }
 
-path "transit/encrypt/payments" {
+path "transit/encrypt/chat" {
   capabilities = ["create","update"]
 }
 EOF
